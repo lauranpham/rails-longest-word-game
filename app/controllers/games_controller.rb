95afce22131c67d31b63389ffdf2b7c85234a86b
Laura Pham <l.pham2311@gmail.com>
2019-11-11 18:25:01 +0000

    Longest word commit

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,34 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
-    @letters = ('a'..'z').to_a.sample(10)
+    @letters = []
+    10.times { @letters << ('A'..'Z').to_a[rand(26)] }
   end
 
   def score
-    @word = params[:word]
-    @letters
+    @attempt = params[:attempt].chars
+    @letters = params[:letters].chars
+    if in_grid?(@attempt, @letters) && english?(@attempt)
+      @result = "Congratulations! #{@attempt.join} is a valid English word"
+    elsif !in_grid?(@attempt, @letters)
+      @result = "Sorry but #{@attempt.join} can't be built out of #{@letters.join}"
+    else
+      @result = "Sorry but #{@attempt.join} does not seem to be a valid English word.."
+    end
+  end
+
+  private
+
+  def in_grid?(attempt, letters)
+    attempt.all? { |letter| attempt.count(letter) <= letters.count(letter) }
+  end
+
+  def english?(attempt)
+    url = "https://wagon-dictionary.herokuapp.com/#{attempt.join}"
+    attempt_serialized = open(url).read
+    attempt = JSON.parse(attempt_serialized)
+    attempt['found']
   end
 end
